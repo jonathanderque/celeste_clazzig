b8760d93310cdb526e95b074e0d98eb410fa6c1d
Jonathan Derque <jonathan.derque@gmail.com>
2024-06-10 20:17:27 +0200

    upgraded nixpkg release to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Celeste Clazzig";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:mitchellh/zig-overlay";
 
